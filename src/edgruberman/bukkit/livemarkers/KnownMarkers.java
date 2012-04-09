00df6399e8117f494877d7a825dee88759548009
EdGruberman <ed@rjump.com>
2012-04-09 00:24:26 -0700

    CHG: Reference marker id history

--- src/edgruberman/bukkit/livemarkers/KnownMarkers.java
+++ src/edgruberman/bukkit/livemarkers/KnownMarkers.java
@@ -1,12 +1,20 @@
 package edgruberman.bukkit.livemarkers;
 
+/**
+ * The first six (0-5) were defined in the original hMod MapMarkers plugin.
+ */
 public enum KnownMarkers {
 
-    ONLINE_PLAYER("4")
-  , OFFLINE_PLAYER("5")
-  , TAMED_WOLF("6")
-  , TAMED_OCELOT("7")
-  , BED_SPAWN("8")
+    SPAWN("0")
+  , HOME("1")
+  , TOWN("2")
+  , PLACE_OF_INTEREST("3")
+  , ONLINE_PLAYER("4")
+  , CAPITAL("5")
+  , OFFLINE_PLAYER("6")
+  , TAMED_WOLF("7")
+  , TAMED_OCELOT("8")
+  , BED_SPAWN("9")
   ;
 
   public final String id;
